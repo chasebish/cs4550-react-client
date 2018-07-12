f8ba140d90f16fe7c9c91b0678d021e8ff2e60a4
chasebish <bishop.c@husky.neu.edu>
2018-07-11 22:52:00 -0400

    added containers & service client

---
+++ src/index.js
@@ -0,0 +1,11 @@
+/* global document */
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
+
+ReactDOM.render(
+    <div>Hello World</div>,
+    document.getElementById('root')
+)
