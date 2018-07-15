8d8e0ba6bbda1c63b98a3aae22d8225fc43c760f
chasebish <bishop.c@husky.neu.edu>
2018-07-15 16:55:36 -0400

    dynamic module list

--- src/containers/ModuleList.jsx
+++ src/containers/ModuleList.jsx
@@ -1,17 +1,38 @@
 import React from 'react'
 
 import ModuleListItem from '../components/ModuleListItem'
-export default class ModuleList
-    extends React.Component {
+export default class ModuleList extends React.Component {
+
+    constructor(props) {
+        super(props)
+    }
+
+    state = {
+        modules: [
+            { title: 'Module 1 - jQuery', id: 123 },
+            { title: 'Module 2 - React', id: 234 },
+            { title: 'Module 3 - Redux', id: 345 },
+            { title: 'Module 4 - Angular', id: 456 },
+            { title: 'Module 5 - Node.js', id: 567 },
+            { title: 'Module 6 - MongoDB', id: 678 }
+        ]
+    }
+
+    renderModuleList() {
+        let modules = this.state.modules
+            .map(function (module) {
+                return <ModuleListItem
+                    title={module.title} key={module.id} />
+            })
+        return modules
+    }
+
     render() {
         return (
             <div>
                 <h3>Module List</h3>
                 <ul className="list-group">
-                    <ModuleListItem title="Module 1"/>
-                    <ModuleListItem title="Module 2"/>
-                    <ModuleListItem title="Module 3"/>
-                    <ModuleListItem title="Module 4"/>
+                    { this.renderModuleList() }
                 </ul>
             </div>
         )
