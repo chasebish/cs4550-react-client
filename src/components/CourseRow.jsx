29895b56017d0708622e476ca2a4dbc222f6ef63
chasebish <bishop.c@husky.neu.edu>
2018-07-17 14:22:37 -0400

    course row -> stateless

--- src/components/CourseRow.jsx
+++ src/components/CourseRow.jsx
@@ -1,35 +1,37 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { Button } from 'react-bootstrap'
 
 import LinkButton from '../components/LinkButton'
 
-export default class CourseRow extends React.Component {
+const CourseRow = (props) => {
 
-    render() {
-        return (
-            <tr>
-                <td>
-                    {this.props.course.title}
-                </td>
-                <td>
-                    {this.props.course.created}
-                </td>
-                <td>
-                    {this.props.course.modified}
-                </td>
-                <td>
-                    <button className="btn btn-danger" onClick={() => { this.props.delete(this.props.course.id) }}>
-                        Delete
-                    </button>
-                    <LinkButton title='Select Course' to={`/course/${this.props.course.id}/edit`} />
-                </td>
-            </tr>
-        )
-    }
+    const formatDateTime = (dateTime) => dateTime.substr(0, 19).replace('T', ' ')
 
+    return (
+        <tr>
+            <td>
+                {props.course.title}
+            </td>
+            <td>
+                {formatDateTime(props.course.created)}
+            </td>
+            <td>
+                {formatDateTime(props.course.modified)}
+            </td>
+            <td>
+                <Button bsStyle='danger' onClick={() => { props.delete(props.course.id) }}>
+                    Delete
+                </Button>
+                <LinkButton title='Select Course' to={`/course/${props.course.id}/edit`} />
+            </td>
+        </tr>
+    )
 }
 
 CourseRow.propTypes = {
     course: PropTypes.object,
     delete: PropTypes.func
 }
+
+export default CourseRow
