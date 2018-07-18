c58ec0bedd97213007df06c39a37b208275d0321
chasebish <bishop.c@husky.neu.edu>
2018-07-18 10:33:15 -0400

    lessons init

--- src/containers/LessonTabs.jsx
+++ src/containers/LessonTabs.jsx
@@ -1,7 +1,60 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import { Tab, Tabs } from 'react-bootstrap'
 
+import LessonService from '../services/LessonService'
+
 export default class LessonTabs extends React.Component {
+
+    constructor(props) {
+        super(props)
+        this.lessonService = LessonService.instance
+    }
+
+    state = {
+        courseId: '',
+        moduleId: '',
+        moduleTitle: '',
+        lessons: [],
+        lesson: {
+            title: ''
+        }
+    }
+
+    componentDidMount() {
+        this.setCourseId(this.props.courseId)
+        this.setModuleId(this.props.moduleId)
+        this.setModuleTitle(this.props.moduleTitle)
+    }
+
+    componentWillReceiveProps(newProps) {
+        this.setCourseId(newProps.courseId)
+        this.setModuleId(newProps.moduleId)
+        this.setModuleTitle(newProps.moduleTitle)
+        this.findAllLessonsForModule(newProps.courseId, newProps.moduleId)
+    }
+
+    setCourseId = () => {
+        this.setState({ courseId: this.props.courseId })
+    }
+
+    setModuleId = () => {
+        this.setState({ moduleId: this.props.moduleId })
+    }
+
+    setModuleTitle = () => {
+        this.setState({ moduleTitle: this.props.moduleTitle })
+    }
+
+    setLessons = (lessons) => {
+        this.setState({ lessons: lessons })
+    }
+
+    findAllLessonsForModule = (courseId, moduleId) => {
+        this.lessonService.findAllLessonsForModule(courseId, moduleId)
+            .then(lessons => this.setLessons(lessons))
+    }
+
     render() {
         return (
             <div>
@@ -16,4 +69,10 @@ export default class LessonTabs extends React.Component {
             </div>
         )
     }
+}
+
+LessonTabs.propTypes = {
+    courseId: PropTypes.string,
+    moduleId: PropTypes.string,
+    moduleTitle: PropTypes.string
 }
