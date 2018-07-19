024c46fa5e050bb4d95cb0b2b150f57d1db086bf
chasebish <bishop.c@husky.neu.edu>
2018-07-19 00:14:12 -0400

    added home

---
+++ src/containers/Home.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { Button, Grid } from 'react-bootstrap'
+
+import './containers.css'
+
+export default class Home extends React.Component {
+
+    render() {
+        return (
+            <Grid>
+
+                <div className='text-center'>
+                    <Button
+                        href='https://whiteboard-server-chasebish.herokuapp.com/index.html'
+                        className='homeButton'
+                        bsStyle='info'
+                        bsSize='large'>
+                        User
+                    </Button>
+
+                    <Link to='/course'>
+                        <Button
+                            className='homeButton'
+                            bsStyle='info'
+                            bsSize='large'>
+                            Courses
+                        </Button>
+                    </Link>
+                </div>
+
+            </Grid>
+        )
+    }
+
+}
--- src/containers/Whiteboard.jsx
+++ src/containers/Whiteboard.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 import { Grid, PageHeader } from 'react-bootstrap'
 
+import Home from './Home'
 import ModuleEditor from './ModuleEditor'
 import CourseEditor from './CourseEditor'
 import CourseList from './CourseList'
@@ -19,7 +20,7 @@ export default class Whiteboard extends React.Component {
                     </PageHeader>
                 </header>
                 <Switch>
-                    {/* <Route exact path='/' component={Home} /> */}
+                    <Route exact path='/' component={Home} />
                     <Route exact path='/course' component={CourseList} />
                     <Route path="/course/:courseId/" component={CourseEditor} />
                     <Route path="/course/:courseId/module/:moduleId" component={ModuleEditor} />
