90c0255ff657d2fe9df1c3d7d53b0083cc9a34ef
chasebish <bishop.c@husky.neu.edu>
2018-07-23 23:05:26 -0400

    sorting course extra credit

--- src/containers/CourseList.jsx
+++ src/containers/CourseList.jsx
@@ -16,6 +16,12 @@ export default class CourseList extends React.Component {
 
     state = {
         courses: [],
+        todayCourses: [],
+        yesterdayCourses: [],
+        weekCourses: [],
+        monthCourses: [],
+        yearCourses: [],
+        oldCourses: [],
         newCourse: {
             title: '',
             owner: ''
@@ -34,6 +40,7 @@ export default class CourseList extends React.Component {
     findAllCourses = () => {
         this.courseService.findAllCourses()
             .then((courses) => {
+                this.binCourses(courses)
                 this.setState({ courses: courses })
             })
     }
@@ -78,6 +85,49 @@ export default class CourseList extends React.Component {
 
     hideModal = () => {
         this.setState({ selectedCourse: null, showModal: false })
+    }
+
+    binCourses = (courses) => {
+
+        let todayCourses = []
+        let yesterdayCourses = []
+        let weekCourses = []
+        let monthCourses = []
+        let yearCourses = []
+        let oldCourses = []
+
+        const now = new Date()
+
+        for (let course of courses) {
+
+            let modified = new Date(course.modified)
+
+            const timeDiff = Math.abs(now.getTime() - modified.getTime())
+            const diffDays = Math.floor(timeDiff / (1000 * 3600 * 24))
+
+            if (diffDays === 0) {
+                todayCourses.push(course)
+            } else if (diffDays === 1) {
+                yesterdayCourses.push(course)
+            } else if (diffDays <= 7) {
+                weekCourses.push(course)
+            } else if (diffDays <= 31) {
+                monthCourses.push(course)
+            } else if (diffDays <= 365) {
+                yearCourses.push(course)
+            } else {
+                oldCourses.push(course)
+            }
+
+            this.setState({
+                todayCourses,
+                yesterdayCourses,
+                weekCourses,
+                monthCourses,
+                yearCourses,
+                oldCourses
+            })
+        }
     }
 
     updateCourse = () => {
@@ -137,7 +187,21 @@ export default class CourseList extends React.Component {
         return rows
     }
 
+    renderSpecificCourseRows = (courses) => {
+
+        if (courses.length === 0) {
+            return
+        }
+
+        let rows = courses.map((course) => {
+            return <CourseRow course={course} key={course.id} showModal={this.showModal} delete={this.deleteCourse} />
+        })
+        return rows
+
+    }
+
     render() {
+
         return (
             <div>
                 <Grid fluid={true}>
@@ -162,20 +226,120 @@ export default class CourseList extends React.Component {
                             </div>
                         </Col>
                     </Row>
-                    <Table bordered hover className='table-responsive'>
-                        <thead>
-                            <tr>
-                                <th>Course Name</th>
-                                <th>Date Created</th>
-                                <th>Date Modified</th>
-                                <th>Owner</th>
-                                <th>Actions</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {this.renderCourseRows()}
-                        </tbody>
-                    </Table>
+                    {this.state.todayCourses.length >= 1 &&
+                        <div>
+                            <h3>Today</h3>
+                            <Table bordered hover className='table-responsive'>
+                                <thead>
+                                    <tr>
+                                        <th>Course Name</th>
+                                        <th>Date Created</th>
+                                        <th>Date Modified</th>
+                                        <th>Owner</th>
+                                        <th>Actions</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {this.renderSpecificCourseRows(this.state.todayCourses)}
+                                </tbody>
+                            </Table>
+                        </div>
+                    }
+                    {this.state.yesterdayCourses.length >= 1 &&
+                        <div>
+                            <h3>Yesterday</h3>
+                            <Table bordered hover className='table-responsive'>
+                                <thead>
+                                    <tr>
+                                        <th>Course Name</th>
+                                        <th>Date Created</th>
+                                        <th>Date Modified</th>
+                                        <th>Owner</th>
+                                        <th>Actions</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {this.renderSpecificCourseRows(this.state.yesterdayCourses)}
+                                </tbody>
+                            </Table>
+                        </div>
+                    }
+                    {this.state.weekCourses.length >= 1 &&
+                        <div>
+                            <h3>This Week</h3>
+                            <Table bordered hover className='table-responsive'>
+                                <thead>
+                                    <tr>
+                                        <th>Course Name</th>
+                                        <th>Date Created</th>
+                                        <th>Date Modified</th>
+                                        <th>Owner</th>
+                                        <th>Actions</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {this.renderSpecificCourseRows(this.state.weekCourses)}
+                                </tbody>
+                            </Table>
+                        </div>
+                    }
+                    {this.state.monthCourses.length >= 1 &&
+                        <div>
+                            <h3>This Month</h3>
+                            <Table bordered hover className='table-responsive'>
+                                <thead>
+                                    <tr>
+                                        <th>Course Name</th>
+                                        <th>Date Created</th>
+                                        <th>Date Modified</th>
+                                        <th>Owner</th>
+                                        <th>Actions</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {this.renderSpecificCourseRows(this.state.monthCourses)}
+                                </tbody>
+                            </Table>
+                        </div>
+                    }
+                    {this.state.yearCourses.length >= 1 &&
+                        <div>
+                            <h3>This Year</h3>
+                            <Table bordered hover className='table-responsive'>
+                                <thead>
+                                    <tr>
+                                        <th>Course Name</th>
+                                        <th>Date Created</th>
+                                        <th>Date Modified</th>
+                                        <th>Owner</th>
+                                        <th>Actions</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {this.renderSpecificCourseRows(this.state.yearCourses)}
+                                </tbody>
+                            </Table>
+                        </div>
+                    }
+                    {this.state.oldCourses.length >= 1 &&
+                        <div>
+                            <h3>Before This Year</h3>
+                            <Table bordered hover className='table-responsive'>
+                                <thead>
+                                    <tr>
+                                        <th>Course Name</th>
+                                        <th>Date Created</th>
+                                        <th>Date Modified</th>
+                                        <th>Owner</th>
+                                        <th>Actions</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {this.renderSpecificCourseRows(this.state.oldCourses)}
+                                </tbody>
+                            </Table>
+                        </div>
+                    }
                 </Grid>
                 <Modal
                     show={this.state.showModal}
